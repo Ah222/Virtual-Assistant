b0087c5e8872f0cec56ec4326c2caa60ece2230f
Your Name <Your Email>
2022-07-03 16:49:18 +0200

    add file data

---
+++ data/keyWords.py
@@ -0,0 +1,47 @@
+MONTHS = ["january", "february", "march", "april", "may", "june",
+          "july", "august", "september", "october", "november", "december"]
+
+DAYS = ["monday", "tuesday", "wednesday",
+        "thursday", "friday", "saturday", "sunday"]
+
+DAY_EXTENTIONS = ["rd", "th", "st", "nd"]
+
+WAKE_WORDS = ['hey computer', 'ok computer', 'olivia',
+              'ok olivia', 'hey olivia', 'hey oliva', 'hey olva']
+
+NAME_KEYWORDS = ["what is your name", "what's your name",
+                 "what should I call you"]
+
+PERSONAL_INFORMATIONS = ["my name: Olivia.", "my mission: to assist you.",
+                         "my favourite colour: probably black but it changes sometimes."]
+
+FEATURES_WORDS = ['what can you do',
+                  'what are you capable of', 'list your features']
+
+LIST_OF_FEATURES = ['I can Search in wikipedia, make a note, set a timer, do basic math, Currency Conversion, Play music, Get the Temperature, Pick a Card for you, Tell you a random Fact, Tell jokes, Play Rock Paper Scissors, Flip a coin for you, Roll a die, Translate to French']
+
+BREAK_WORDS = ["break", "bye", "goodbye", "shut down"]
+
+TODAY_DATE = ["what is today", "what is the date", "what is the day"]
+
+NOTE_STRS = ["make a note", "write this down", "remember this", "type this"]
+
+SEARCH_KEY_WORDS = ["who is", "what is"]
+
+TRANSLATE_KEY_WORDS = ["translate to french", "translation"]
+
+TIMER_KEY_WORDS = ['set a timer', 'set an alarm']
+
+CANCEL_TIMER_KEY_WORDS = ['cancel timer',
+                          'cancel the timer', 'cancel alarm', 'cancel the alarm']
+
+CUURENCY_KEY_WORDS = ['convert from']
+
+JOKES_KEY_WORDS = ['tell me a joke', 'can you tell me a joke', 'say a joke']
+
+CANCEL_MUSIC_KEY_WORDS = ['cancel music',
+                          'music stop', 'stop music', 'music cancel']
+
+
+MATH_WORDS = ["plus", "+", "Subtract", "-", "mins",
+              "multiplied", "multiply", "divided", "/", "power", "*"]
